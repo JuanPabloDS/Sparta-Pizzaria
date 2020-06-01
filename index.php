47eca85008be61e16fad9991bb1e89fe1c33dda8
JuanPabloDS <jpsevero30@hotmail.com>
2020-06-01 19:50:27 -0300

    Iniciando o repositorio

---
+++ index.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<head lang="pt-br">
+	<meta charset=utf-8>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>SPARTA PIZZARIA</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/estilo.css">
+</head>
+<body class="">
+	<div class="principal">
+
+	<div class="menu ">
+			<nav class="navbar navbar-expand-lg ">
+					<div class="collapse navbar-collapse navegacao" id="conteudoNavbarSuportado">
+	    			<ul class="navbar-nav mr-auto">
+	      			<li class="nav-item dropdown">
+	        		<a class="nav-link dropdown-toggle btn-outline-warning cor" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	       			<i class="fas fa-bars"></i>
+	        		</a>
+	        		<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+				        <a class="dropdown-item" href="index.php">Exemplo</a>
+				        <a class="dropdown-item" href="day2.php">Exemplo</a>
+				        <a class="dropdown-item" href="day3.php">Exemplo</a>
+				        <a class="dropdown-item" href="day4.php">Exemplo</a>
+				        <a class="dropdown-item" href="day5.php">Exemplo</a>
+	        		</div>
+	      			</li>
+	      			<li class="nav-item active">
+	        			<a class="nav-link btn-outline-warning cor"  href="index.php">SPARTA PIZZARIA <span class="sr-only">(página atual)</span></a>
+	      			</li>
+	    			</ul>
+	    			
+	    			<form class="form-inline cor" >
+	      			<input class="form-control  pesquisa cor   " type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+	      			<button class="btn btn-outline-warning cor backcor" type="submit"><i class="fas fa-search"></i></button>
+	      			<li class="nav-item active login">
+	        			<a class="nav-link btn-outline-warning login"  href="index.php"><i class="far fa-user"></i> <span class="sr-only">(página atual)</span></a>
+	      			</li>
+	    			</form>
+	    			
+  				</div>
+			</nav>
+			<div class="menu2">
+				<form>
+                <button class="reserva responsive">RESERVE SEU LOCAL</button>
+            	</form>
+                  <img class="logo" src="assets\img\logo.png" class="logo" alt="Logo" title="Logo Sparta Pizzaria">     
+            	<br>
+            	<br>
+            	<form>
+                <button class="reserva responsive">FAÇA SEU PEDIDO</button>
+                </form>
+				
+                  
+			</div>
+	</div>
+			<div class="slide">
+				<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+					  <ol class="carousel-indicators">
+					    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+					    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+					    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+					  </ol>
+					  <div class="carousel-inner">
+					    <div class="carousel-item active">
+					      <img class="d-block w-100 slide" src="assets\img\slide1.jpg">
+					    </div>
+					    <div class="carousel-item">
+					      <img class="d-block w-100 slide" src="assets\img\slide2.jpg" alt="Segundo Slide">
+					    </div>
+					    <div class="carousel-item">
+					      <img class="d-block w-100 slide" src="assets\img\slide3.jpg" alt="Terceiro Slide">
+					    </div>
+					  </div>
+					  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+					    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					    <span class="sr-only">Anterior</span>
+					  </a>
+					  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+					    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+					    <span class="sr-only">Próximo</span>
+					  </a>
+					</div>
+			</div>
+			<div>
+				<table class="table table-bordered">
+				  <thead>
+				    <tr>
+				      <th class="menu3" scope="col"><a href="cardapio.php"><font color="black"><img class=" imagem " src="assets\img\cardapio.jpg"><br><br>CARDÁPIO</a></th>
+				      <th class="menu3" scope="col"><a href="cardapio.php"><font color="black"><img class=" imagem " src="assets\img\rodizio.jpg"><br><br>RODIZIO</a></th>
+				      <th class="menu3" scope="col"><a href="cardapio.php"><font color="black"><img class=" imagem " src="assets\img\delivery.jpg"><br><br>DELIVERY</a></th>
+				      <th class="menu3" scope="col"><a href="cardapio.php"><font color="black"><img class=" imagem " src="assets\img\novidades.jpg"><br><br>NOVIDADES</a></th>
+				    </tr>
+				  </thead>
+				</table>
+			</div>
+				<br><br>
+				<br><br>
+			<div>
+				<table class="table table-bordered">
+				  <thead>
+				    <tr scope="col">
+				       
+				      <h4 class="texto">
+				      Os melhores e mais diversificados sabores de Pizza são a especialidade da casa, contamos com a maior variedade premium de Pizzas constante do nosso cardápio.
+
+						Venha se deliciar e explorar os nossos sabores exclusivos
+					</h4>
+					<h4 class="texto" align="carousel-control-next">
+					Venha se deliciar e explorar os nossos sabores exclusivos.
+					</h4>
+					<br><br>
+					<h4 align="center">PROMOÇÕES</h4>
+					<br><br>
+				    </tr>
+				  </thead>
+				</table>
+				<table class=" pizza ">
+				  <thead>
+				    <tr>
+				      <th class="" scope="col">
+				      	<form >
+				      	<img class="imagem2" src="assets\img\basca.png">
+				      	<h3 class="pizzatexto">BASCA</h3>
+				      	<h5 class="pizzatexto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus justo ligula, aliquam quis commodo et,ipsum</h5>
+				      	</form>
+				      </th>
+				    </tr>
+				  </thead>
+				</table>
+				<table class="pizza ">
+				  <thead>
+				    <tr>
+				      <th class="" scope="col">
+				      	<form >
+				      	<img class="imagem2" src="assets\img\basca.png">
+				      	<h3 class="pizzatexto">BASCA</h3>
+				      	<h5 class="pizzatexto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus justo ligula, aliquam quis commodo et,ipsum</h5>
+				      	</form>
+				      </th>
+				    </tr>
+				  </thead>
+				</table>
+				<table class="pizza ">
+				  <thead>
+				    <tr>
+				      <th class="" scope="col">
+				      	<form >
+				      	<img class="imagem2" src="assets\img\basca.png">
+				      	<h3 class="pizzatexto">BASCA</h3>
+				      	<h5 class="pizzatexto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus justo ligula, aliquam quis commodo et,ipsum</h5>
+				      	</form>
+				      </th>
+				    </tr>
+				  </thead>
+				</table>
+				<table class="pizza ">
+				  <thead>
+				    <tr>
+				      <th class="" scope="col">
+				      	<form >
+				      	<img class="imagem2" src="assets\img\basca.png">
+				      	<h3 class="pizzatexto">BASCA</h3>
+				      	<h5 class="pizzatexto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus justo ligula, aliquam quis commodo et,ipsum</h5>
+				      	</form>
+				      </th>
+				    </tr>
+				  </thead>
+				</table>
+				<table class="pizza ">
+				  <thead>
+				    <tr>
+				      <th class="" scope="col">
+				      	<form >
+				      	<img class="imagem2" src="assets\img\basca.png">
+				      	<h3 class="pizzatexto">BASCA</h3>
+				      	<h5 class="pizzatexto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus justo ligula, aliquam quis commodo et,ipsum</h5>
+				      	</form>
+				      </th>
+				    </tr>
+				  </thead>
+				</table>
+				<table class="pizza ">
+				  <thead>
+				    <tr>
+				      <th class="" scope="col">
+				      	<form >
+				      	<img class="imagem2" src="assets\img\basca.png">
+				      	<h3 class="pizzatexto">BASCA</h3>
+				      	<h5 class="pizzatexto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus justo ligula, aliquam quis commodo et,ipsum</h5>
+				      	</form>
+				      </th>
+				    </tr>
+				  </thead>
+				</table>
+			</div>
+			<div>
+
+			</div>
+				
+
+
+	</div>
+
+
+
+
+<script type="text/javascript" src="assets/js/jquery-3.4.1.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.bundle.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.bundle.js"></script>
+</body>
+</html>
